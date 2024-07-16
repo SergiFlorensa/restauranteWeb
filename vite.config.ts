3464c86fa909b5124f4e06bcfa4728a018d2ffdb
Sergi Florensa <sergiflorensadj@gmail.com>
2024-07-16 18:08:05 +0200

    prueba

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/restauranteWeb/patiocriollo/', // Ajusta esta ruta según la estructura de tu proyecto
 
 })
