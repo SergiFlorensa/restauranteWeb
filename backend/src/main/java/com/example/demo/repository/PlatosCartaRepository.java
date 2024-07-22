34bfab2d0a16e87565f7e4feeedb45dad4f8fdb3
Sergi Florensa <sergiflorensadj@gmail.com>
2024-07-22 19:51:05 +0200

    last version

--- backend/src/main/java/com/example/demo/repository/PlatosCartaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.PlatosCarta;
-
-public interface PlatosCartaRepository extends JpaRepository<PlatosCarta, Long> {
-    // Métodos específicos del repositorio, si los necesitas
-}
